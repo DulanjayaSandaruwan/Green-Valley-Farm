d18d1a9e027a60d87b2154eb1d5dbc84fa7aa1a0
DulanjayaSandaruwan <dulanjayasandaruwan1998@gmail.com>
2021-09-19 02:50:21 +0530

    Add Notifications and Create Order Details

--- src/controller/NotificationFormController.java
+++ src/controller/NotificationFormController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import db.DBConnection;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
@@ -17,38 +18,60 @@ import java.util.ArrayList;
  **/
 public class NotificationFormController {
 
+    public static ArrayList<Products> products = new ArrayList<>();
     public AnchorPane root7;
     public VBox vBoxNotification;
-    public static ArrayList<Products> products = new ArrayList<>();
 
     public void initialize() {
         showNotifications();
+        loadNotification();
+    }
 
+    public void loadNotification() {
+        int index = 0;
+
+        int row = 0;
 
         for (Products productsDetail : products) {
-            Label label = new Label();
-            label.setStyle("-fx-font-size: 50");
-            label.setText(productsDetail.getProductId());
-            AnchorPane anchorPane = new AnchorPane(label);
-            AnchorPane anchorPane2 = new AnchorPane(new javafx.scene.control.Label("    "));
-            anchorPane.setStyle("-fx-background-color: #2D75E8");
-            anchorPane.setStyle("-fx-max-height: 5420");
 
-            products.size();
+            Label label = new Label();
+            Button button = new Button("Clear");
+            label.setStyle("-fx-font-size: 18");
+            label.setText("The production" + " volume of " + productsDetail.getProductId() +
+                    " is declining in" + "the warehouses." + "\n" + "Be aware of that.");
+            button.setStyle("-fx-background-color: red; -fx-translate-x: 850; -fx-max-height: 100; -fx-min-height: 50 ");
+
+            button.setId("" + ++index);
+            button.setOnAction(event -> {
+                Object node = event.getSource();
+                Button b = (Button) node;
+                int remove = Integer.parseInt(b.getId());
+                products.remove(--remove);
+                vBoxNotification.getChildren().clear();
+                loadNotification();
+            });
+
+            AnchorPane anchorPane = new AnchorPane(label, button);
+            AnchorPane anchorPane2 = new AnchorPane(new javafx.scene.control.Label("    "));
+            if ((++row % 2 == 0)) {
+                anchorPane.setStyle("-fx-background-color: #1fdedb");
+            } else {
+                anchorPane.setStyle("-fx-background-color: #a5ecd7");
+            }
 
             vBoxNotification.getChildren().addAll(anchorPane);
             vBoxNotification.getChildren().addAll(anchorPane2);
         }
-
     }
 
     private void showNotifications() {
         try {
             products.clear();
+
             PreparedStatement preparedStatement
                     = DBConnection.getInstance().getConnection().prepareStatement("select * from finalProduct where qtyOnHand < 5000");
             ResultSet resultSet = preparedStatement.executeQuery();
-            if(resultSet.next()){
+            while (resultSet.next()) {
                 Products productsDetail = new Products(resultSet.getString("finalProductId"),
                         resultSet.getString("finalProductName"),
                         resultSet.getString("finalProductType"),
@@ -61,5 +84,4 @@ public class NotificationFormController {
             throwables.printStackTrace();
         }
     }
-
 }
--- src/controller/ReportsFormController.java
+++ src/controller/ReportsFormController.java
@@ -1,8 +1,23 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
  * @author : D.D.Sandaruwan <dulanjayasandaruwan1998@gmail.com>
  * @Since : 2021-09-08
  **/
 public class ReportsFormController {
+    public void btnOrderDetailsOnAction(ActionEvent actionEvent) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource("../view/OrderDetailsForm.fxml"));
+        Scene scene = new Scene(load);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
